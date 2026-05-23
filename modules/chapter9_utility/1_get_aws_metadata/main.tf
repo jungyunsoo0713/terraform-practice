bb8cfc3594d387e4f3984279a264ead5bec99b07
Young <jungyunsoo0713@gmail.com>
2026-05-23 15:05:06 +0900

    Initial commit!

---
+++ modules/chapter9_utility/1_get_aws_metadata/main.tf
@@ -0,0 +1,28 @@
+# 데이터 블록 정의
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+data "aws_iam_account_alias" "current" {}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# 출력값
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "account_alias" {
+  value = data.aws_iam_account_alias.current.account_alias
+}
+
+output "region_name" {
+  value = data.aws_region.current.name
+}
+
+output "region_code" {
+  value = split("-", data.aws_availability_zones.available.zone_ids[0])[0]
+}
+
+output "az_names" {
+  value = data.aws_availability_zones.available.names
+}
---
+++ modules/chapter9_utility/2_check_aws_cross_provider/main.tf
@@ -0,0 +1,49 @@
+# 프로바이더 a
+data "aws_caller_identity" "a" {
+  provider = aws.a
+}
+data "aws_region" "a" {
+  provider = aws.a
+}
+
+# 프로바이더 b
+data "aws_caller_identity" "b" {
+  provider = aws.b
+}
+data "aws_region" "b" {
+  provider = aws.b
+}
+
+locals {
+  a_account_id = data.aws_caller_identity.a.account_id
+  b_account_id = data.aws_caller_identity.b.account_id
+
+  a_region = data.aws_region.a.name
+  b_region = data.aws_region.b.name
+}
+
+# 출력값
+output "is_cross_account" {
+  value = local.a_account_id != local.b_account_id
+}
+
+output "is_cross_region" {
+  value = local.a_region != local.b_region
+}
+
+# 추가 출력값
+output "a_account_id" {
+  value = local.a_account_id
+}
+
+output "b_account_id" {
+  value = local.b_account_id
+}
+
+output "a_region" {
+  value = local.a_region
+}
+
+output "b_region" {
+  value = local.b_region
+}
